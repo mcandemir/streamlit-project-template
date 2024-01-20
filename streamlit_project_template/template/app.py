7b6595d6629a9af4a5cda9ff0eb0d05a68047a19
Can Demir <mhmtcndmr01@gmail.com>
2024-01-21 00:39:02 +0300

    add example app.py code layout

--- streamlit_project_template/template/app.py
+++ streamlit_project_template/template/app.py
@@ -1,6 +1,23 @@
 import streamlit as st
-from pages import HomePage
+from pages import HomePage, MyPage1, MyPage2
 
 
 # page config
 st.set_page_config(page_title="My Project", layout="wide", initial_sidebar_state="collapsed")
+
+
+# set session states
+if 'selected_page' not in st.session_state:
+    st.session_state['selected_page'] = 'home'
+
+
+# page navigation
+match st.session_state['selected_page']:
+    case 'home':
+        HomePage.loadHomePage()
+
+    case 'mypage1':
+        MyPage1.loadMyPage1()
+    
+    case 'mypage2':
+        MyPage2.loadMyPage2()
