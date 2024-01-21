d7bb0c96192189b60d472fe1fc51598630789d65
Can Demir <mhmtcndmr01@gmail.com>
2024-01-21 15:59:13 +0300

    bugfix dockerfile

--- streamlit_project_template/createtemplate.py
+++ streamlit_project_template/createtemplate.py
@@ -80,7 +80,7 @@ EXPOSE 8501
 
 HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
 
-ENTRYPOINT ["streamlit", "run", "streamlit_app.py", "--server.port=8501", "--server.address=0.0.0.0"]""")
+ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]""")
 
 
     def _create_requirements(self):
